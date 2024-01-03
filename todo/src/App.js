d3e9372a6a12dc4ec3b8d2d6685d84f5be923fce
Your Name <you@example.com>
2024-01-03 10:40:21 +0300

    starting

---
+++ todo/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.first_container}>
+        <Text style={styles.title}>Yapılacaklar</Text>
+        <Text style={styles.number}>0</Text>
+      </View>
+    </View>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#102027',
+  },
+  first_container: {
+    color: '#FFA500',
+    marginTop: 35,
+    marginLeft: 10,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#FFA500',
+    fontSize: 50,
+    fontWeight: 'bold',
+  },
+  number: {
+    color: '#FFA500',
+    fontSize: 50,
+    fontWeight: 'bold',
+    marginLeft: 20,
+  },
+});
